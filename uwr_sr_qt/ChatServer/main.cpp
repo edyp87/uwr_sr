46d084008aadbda3ceb5ec89869cab70aa294c58
Marek <edyp87@gmail.com>
2014-05-17 23:34:21 +0200

    uwr_sr_qt added2

--- uwr_sr_qt/ChatServer/main.cpp
+++ uwr_sr_qt/ChatServer/main.cpp
@@ -1,22 +1,11 @@
 #include <QCoreApplication>
 #include <QHostAddress>
 #include "chatserver.h"
-
-static const quint16 DEFAULT_PORT = 6667;
-
 int main(int argc, char* argv[])
 {
 	QCoreApplication a(argc, argv);
-	quint16 port = 0;
-	bool ok = false;
-	if (argc > 0)
-	{
-		port = QString(argv[0]).toInt(&ok);
-	}
-	if (!ok)
-	{
-		port = DEFAULT_PORT;
-	}
+	quint16 port = 5432;
+
     ChatServer server;
 	server.listen(QHostAddress::Any, port);
 	return a.exec();
